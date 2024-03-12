2607fc9bdc8c7efc8ff90eb6f1fb098291614281
smit-darji <smit.softvan@gmail.com>
2024-03-12 12:40:37 +0530

    test

--- a.py
+++ a.py
@@ -1,10 +1,12 @@
 import sys
 
 def main():
+    file_list= []
     added_files = sys.argv[1:]  # Get command line arguments except the script name
     print("List of added files:")
     for file in added_files:
-        print(file)
+        file_list.append(file)
+    print("List print from python", file_list)
 
 if __name__ == "__main__":
     main()
