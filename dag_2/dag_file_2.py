665c6eb5c825498cfa9af9c59a252996011f4229
smit-darji <smit.softvan@gmail.com>
2024-03-12 11:35:13 +0530

    first commit

--- dag_2/dag_file_2.py
+++
@@ -1,8 +0,0 @@
-def example_function_2():
-    try:
-        result = int("hello")
-        print("Result:", result)
-    except ValueError as e:
-        print("Error:", e)
-    except TypeError as e:
-        print("Error:", e)
